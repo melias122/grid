e95a2ad82d48d34db158d503bd8283392ea7ad68
Martin Elias <melias122@gmail.com>
2017-03-20 23:48:46 +0100

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
 CXX=mpicxx
-
 BUILD_DIR=build
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
-cmake ..
-make -j $(nproc)
+usage() {
+	echo "Usage: $0 [-a] [-r project]" 1>&2
+	echo "   -a: builds all projects" 1>&2
+	echo "   -r: runs project" 1>&2
+	exit 1
+}
+
+build_all() {
+	mkdir -p $BUILD_DIR
+	cd $BUILD_DIR
+	cmake ..
+	make -j $(nproc)
+}
+
+#run() {
+#	./$BUILD_DIR/
+#}
+
+while getopts ":ar:" opt; do
+	case "${opt}" in
+		a) build_all
+		   ;;
+		r) run opt
+		   ;;
+		*) usage
+		   ;;
+	esac
+done
+shift $((OPTIND-1))
+
