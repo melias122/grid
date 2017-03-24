a1fa1b2ee6b407a135290abc5cf192aee12bb13c
Martin Elias <melias122@gmail.com>
2017-03-24 17:37:21 +0100

    clang-format

--- src/app.h
+++ src/app.h
@@ -3,48 +3,47 @@
 
 #include <string>
 
-class Application
-{
+class Application {
 public:
-	Application();
-	Application(int &argc, char **&argv);
-	~Application();
+    Application() {}
+    Application(int argc, char** argv);
+    ~Application();
 
-	int rank();
-	int size();
+    int rank();
+    int size();
+
 private:
-	int m_rank, m_size;
+    int m_rank, m_size;
 };
 
-struct Bruteforcer
-{
-	~Bruteforcer() {}
-	// notice that procId count from 0
-	// procId is always < nproc
-	virtual void run(int procId, int nproc) = 0;
+class Bruteforcer {
+public:
+    ~Bruteforcer() {}
+
+    // notice that procId count from 0
+    // procId is always < nproc
+    virtual void run(int procId, int nproc) = 0;
 };
 
-struct Runner
-{
-	Runner() = delete;
+struct Runner {
+    Runner() = delete;
 
-	static void run(Bruteforcer *b)
-	{
-		Application app;
-		b->run(app.rank(), app.size());
-	}
+    static void run(Bruteforcer* b)
+    {
+        Application app;
+        b->run(app.rank(), app.size());
+    }
 };
 
-void sendString(int dest, int tag, const std::string &s);
+void sendString(int dest, int tag, const std::string& s);
 std::string recvString(int source, int tag);
 
 // experimental communicator
 template <typename T>
-struct comm
-{
+struct comm {
 public:
-	void send(int dest, int tag, const T &data, int size = 1);
-	T recv(int source, int tag, int size = 1) const;
+    void send(int dest, int tag, const T& data, int size = 1);
+    T recv(int source, int tag, int size = 1) const;
 };
 
 template struct comm<char>;
